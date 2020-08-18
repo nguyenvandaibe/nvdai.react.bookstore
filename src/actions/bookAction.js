9a38a6981db3471c5eb6402e33f2a10305939341
Nguyen Van Dai <nguyenvandaiBe@gmail.com>
2020-08-18 16:06:48 +0700

    Lay sach thanh cong

--- src/actions/bookAction.js
+++ src/actions/bookAction.js
@@ -1,18 +1,15 @@
 import { FETCH_BOOKS } from "./types";
+import axios from 'axios';
 
 export const fetchBooks = () => async dispatch => {
-    try{
+    try {
         const res = await axios.get(`https://localhost:44372/api/books`)
-        dispatch( {
+        dispatch({
             type: FETCH_BOOKS,
             payload: res.data
         })
     }
-    catch(e){
-        // dispatch( {
-        //     type: USERS_ERROR,
-        //     payload: console.log(e),
-        // })
+    catch (e) {
         throw (e);
     }
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -11,7 +11,44 @@ class BookList extends Component {
         console.log(books);
 
         return (
-            
+            <div>
+                <h1>Book list</h1>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Category</th>
+                            <th>Author</th>
+                            <th>Author</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            books.map((book, index) => {
+                                console.log(book)
+                                return (
+                                    <tr key={index} no={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{book.Name}</td>
+                                        <td>{book.Price}</td>
+                                        <td>{book.Category}</td>
+                                        <td>{book.Author}</td>
+                                        <td>
+                                            <button className="btn btn-danger">Remove</button>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
+
+const mapStateToProps = (state) => ({ books: state.books });
+
+export default connect(mapStateToProps, { fetchBooks })(BookList);
